b35d386b2265c9f184110a051c368ae69e4b4b71
Michael Valladolid <mvalladolid@indeed.com>
2022-10-31 19:37:59 +0900

    simple logs

--- src/main/java/com/jvm/benchmark/controller/UserController.java
+++ src/main/java/com/jvm/benchmark/controller/UserController.java
@@ -4,6 +4,8 @@ import com.jvm.benchmark.controller.model.ImmutableUserView;
 import com.jvm.benchmark.controller.model.UserView;
 import com.jvm.benchmark.domain.model.User;
 import com.jvm.benchmark.domain.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     public UserController(final UserService userService) {
@@ -20,6 +23,7 @@ public class UserController {
     @GetMapping(value = "/")
     UserView getUser() {
         final User user = userService.getUser();
+        LOGGER.info("Get user: [{}]", user);
         return ImmutableUserView.builder()
                 .username(user.username())
                 .build();
